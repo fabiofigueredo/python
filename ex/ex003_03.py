bb2f42d7f564694c597917ff37c85d4c66642192
fabiofigueredo <fabiochexx@gmail.com>
2018-10-27 14:10:59 -0300

    Exercicios 03 do curso de Python, repeticao para reforco de conceito

---
+++ ex/ex003_03.py
@@ -0,0 +1,4 @@
+n1 = int(input('Insira o primeiro valor: '))
+n2 = int(input('Insira o segundo valor: '))
+soma = n1*n2
+print('O resultado da multiplicação entre os números {} e {}, é {}!'.format(n1, n2, soma))
