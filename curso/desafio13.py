8275faa6eb279256668bf011bc2d70d5a1ea680e
fabiofigueredo <fabiochexx@gmail.com>
2018-10-30 18:41:08 -0300

    Melhoria no código do desafio 13, realizando o calculo inteiro em uma linha

--- curso/desafio13.py
+++ curso/desafio13.py
@@ -2,6 +2,6 @@
 
 sal = float(input('Digite seu salário: '))
 perc = float(input('Digite o percentual do aumento (sem %): '))
-aum = (sal*perc)/100
-salfinal = sal + aum
-print('O seu salário de R${:.2f} com {:.2f}% de aumento vai para R${:.2f}.'.format(sal, perc, salfinal))
+aum = ((sal*perc)/100)+sal
+# salfinal = sal + aum
+print('O seu salário de R${:.2f} com {:.2f}% de aumento vai para R${:.2f}.'.format(sal, perc, aum))
